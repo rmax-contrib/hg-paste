3f08b1786f9334252beaa8dc10315e460a0ce9fe
Steve Losh <steve@stevelosh.com>
2009-09-14 21:33:34 -0400

    Add more structure.

--- paste.py
+++ paste.py
@@ -4,22 +4,34 @@
 '''
 
 from mercurial import util
+from urllib2 import urlopen
+
+
+def _paste_dpaste(**parameters):
+    pass
 
 pastebins = {
-    'dpaste': 'http://dpaste.com/api/v1/'
+    'dpaste': { 'url': 'http://dpaste.com/api/v1/',
+                'parameters': {
+                    'required': ['code'],
+                    'optional': ['title'], },
+                'handler': _paste_dpaste,
+    }
 }
 
 def paste(ui, repo, destination, **opts):
     if destination not in pastebins:
         raise util.Abort('Unknown pastebin.  See "hg help paste" for supported pastebins.')
         return
-    pass
+    
+    
 
 
 cmdtable = {
     "paste": 
     (paste, [
         ('d', 'destination', '', 'the pastebin site to use'),
+        ('t', 'title', '', 'the title of the paste (optional)'),
     ],
     'hg paste -d PASTEBIN')
 }
